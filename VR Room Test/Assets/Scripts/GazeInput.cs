45606835350d20aaaf5b3c44f4d7144d01d9fed8
Emi Brown <emibrown55@gmail.com>
2019-05-12 19:28:35 -0500

    Version 1.0
    - GVR 1.2
    - Runs on iOS & Google Cardboard
    - Room model with basic shapes to represent contents (shows open space more clearly)
    - Tap/click to change between Jack and Ma's heights
    - Gaze input for Skylight and Lamp to change lighting

---
+++ VR Room Test/Assets/Scripts/GazeInput.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeInput : MonoBehaviour {
+    private GameObject sun;
+    private GameObject lamp;
+    private GameObject sky;
+    private GameObject shade;
+
+    public Material bright;
+    public Material dark;
+    public Material shadeUnlit;
+    public Material shadeLit;
+
+    private float timer;
+    private float gazeTime = 2.0f;
+
+    private bool isGazedAt;
+
+    // --- Initialization ---
+    void Start() {
+        sun = GameObject.Find("Sun");
+        lamp = GameObject.Find("LampLight");
+        sky = GameObject.Find("Sky");
+        shade = GameObject.Find("Shade");
+
+        timer = 0.0f;       //resets the timer
+    }
+
+    //--- Main Loop ---
+    void Update() {
+        if (isGazedAt) {
+            //activates while the player is selecting an object with the reticle pointer
+            timer += Time.deltaTime;   //starts timer
+            if (timer >= gazeTime) {
+                //activates after the object is gazed at for 2 seconds
+                PointerGaze();
+                timer = 0.0f;  //resets the timer
+            }
+        }
+    }
+
+    //--- Functions ---
+    //- For Event Trigger -
+    public void PointerEnter() {
+        //activates when the reticle pointer is over an object
+        isGazedAt = true;  //player is gazing at an object
+    }
+    public void PointerExit() {
+        //activates when the reticle pointer is not over an object
+        isGazedAt = false;  //player is not gazing at anything
+        timer = 0.0f;       //resets timer
+    }
+    public void PointerGaze() {
+        //lamp
+        if(gameObject.name == "Shade") {
+            if (lamp.activeSelf) {
+                lamp.SetActive(false);
+                shade.GetComponent<MeshRenderer>().material = shadeUnlit;
+            }
+            else {
+                lamp.SetActive(true);
+                shade.GetComponent<MeshRenderer>().material = shadeLit;
+            }
+        }
+        else if(gameObject.name == "Sky") {
+            if (sun.activeSelf) {
+                sun.SetActive(false);
+                sky.GetComponent<MeshRenderer>().material = dark;
+            }
+            else {
+                sun.SetActive(true);
+                sky.GetComponent<MeshRenderer>().material = bright;
+            }
+        }
+    }
+}
---
+++ VR Room Test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public bool isJack;
+
+    private float heightJack = 0.94f; //average height of  a 3 year old girl (Bronwyn is the same size as Jack)
+    private float heightMa = 1.61798f; //average height for an adult woman
+
+    // --- Initialization ---
+    void Start() {
+        //set inital player position
+        setPlayerPosition();
+    }
+
+    // --- Main Loop ---
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            changePlayer();
+            changePlayerHeight();
+        }
+    }
+
+    //set player settings 
+    public void setPlayerPosition() {
+        transform.eulerAngles = new Vector3(0, 90, 0);
+        if(isJack) transform.position = new Vector3(-1.0f, heightJack, -0.5f);
+        else transform.position = new Vector3(-1.0f, heightMa, -0.5f);
+    }
+    //change player
+    public void changePlayer() {
+        if(isJack) isJack = false;
+        else isJack = true;
+    }
+    //change player height
+    public void changePlayerHeight() {
+        if(isJack) transform.position = new Vector3(transform.position.x, heightJack, transform.position.z);
+        else transform.position = new Vector3(transform.position.x, heightMa, transform.position.z);
+    }
+}
